f84bf2f2fc14de61754e9f79a20a85b19e3c122f
Andrey Filippov <solo.framework@gmail.com>
2011-06-20 13:55:16 +0400

    добавлен компонент для управления скриптами, которые подключаются в HTML страницу

---
+++ lib/Web/ClientScript.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ *
+ *
+ * PHP version 5
+ *
+ * @package
+ * @author  Andrey Filippov <afi@i-loto.ru>
+ */
+
+class ClientScript
+{
+	/**
+	 * Версия файла
+	 *
+	 * @var string
+	 */
+	public $revision = "";
+
+	/**
+	 * Суффикс файла.
+	 * Применяется для подключения минимизированных скриптов
+	 * Только для Javascript
+	 *
+	 * @var string
+	 */
+	public $fileSuffix = null;
+
+	/**
+	 * Список загруженных скриптов
+	 *
+	 * @var array
+	 */
+	private $scripts = array();
+
+	public function __construct()
+	{
+
+	}
+
+
+	/**
+	 * Осуществляет контроль загрузки JS и CSS на страницу
+	 *
+	 * @param string $fileName Имя скрипта
+	 *
+	 * @return bool
+	 */
+	public function load($fileName)
+	{
+		$hash = md5($fileName);
+
+		if (!in_array($hash, $this->scripts))
+		{
+			$this->scripts[] = $hash;
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+}
+?>
---
+++ tools/application/app/smarty.plugins/function.clientscript.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Вставляет в HTML теги для подключения JS и CSS скриптов
+ * Выполняет контроль загрузки JS и CSS файлов,
+ * чтобы одинаковые файлы не подгружались несколько раз
+ *
+ * @example
+ * {loadscript file='js/example.js' type='js' [media]}
+ * {loadscript file='js/example.css' type='css' [media]}
+ *
+ * PHP version 5
+ *
+ * @author   Andrey Filippov <afi@i-loto.ru>
+ */
+
+function smarty_function_clientscript($params, $smarty)
+{
+	$loader = Application::getInstance()->getComponent("clientscript");
+	$res = $loader->load($params["file"]);
+	if ($res === false)
+		return "";
+
+	$type = $params['type'];
+	$fileName = $params['file'];
+	$media = $params['media'] ? "media=\"" . $params['media'] . "\" " : "";
+	$revision = $loader->revision;
+	$fileSuffix = $loader->fileSuffix;
+
+	if ($fileSuffix != null)
+	{
+		$path = pathinfo($fileName);
+		$fileName = $path["dirname"] . $path["filename"] . $fileSuffix . $path["extension"];
+	}
+
+	if($type == "js")
+		return "<script language=\"javascript\" type=\"text/javascript\" src=\"{$fileName}?{$revision}\"></script>";
+	if($type == "css")
+		return "<link type=\"text/css\" href=\"{$fileName}?{$revision}\" rel=\"stylesheet\" {$media}/>";
+	if($type == "swf")
+		return "{$fileName}?{$revision}";
+}
+?>
