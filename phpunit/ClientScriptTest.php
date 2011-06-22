7d67e0de0527ce1ca6fec6af297a08faea0d2421
Andrey Filippov <solo.framework@gmail.com>
2011-06-22 10:53:36 +0400

    Добавлен тест для ClientScript

---
+++ phpunit/ClientScriptTest.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Тестирование класса ClientScript
+ *
+ * PHP version 5
+ *
+ * @package
+ * @author  Andrey Filippov <afi@i-loto.ru>
+ */
+
+require_once 'PHPUnit/Framework/TestCase.php';
+require_once 'lib/Web/ClientScript.php';
+
+class ClientScriptTest extends PHPUnit_Framework_TestCase
+{
+	/**
+	 * Экземпляр ClientScript
+	 *
+	 * @var ClientScript
+	 */
+	private $cs = null;
+
+	/**
+	 * Prepares the environment before running a test.
+	 */
+	protected function setUp()
+	{
+		parent::setUp();
+		$this->cs = new ClientScript();
+	}
+
+	/**
+	 * Cleans up the environment after running a test.
+	 */
+	protected function tearDown()
+	{
+		parent::tearDown();
+	}
+
+	/**
+	 *
+	 *
+	 * @return
+	 */
+	public function test_load()
+	{
+		// Подключен впервые
+		$res = $this->cs->load("first.js");
+		$this->assertTrue($res);
+
+		// пробуем еще раз
+		$res = $this->cs->load("first.js");
+		$this->assertFalse($res);
+
+		// теперь другой
+		$res = $this->cs->load("another.js");
+		$this->assertTrue($res);
+	}
+}
+?>
