b7f6f83b94324b5ab77953d5b5654bdfeedd49c2
Andrey Filippov <afi.work@gmail.com>
2013-03-11 19:30:31 +0400

    Первая рабочая версия утилиты для тестирования настроек маршрутизации

--- tools/testroute.php
+++ tools/testroute.php
@@ -9,15 +9,34 @@
  */
 
 require_once "../Solo/Core/Route.php";
+require_once "../Solo/Core/Request.php";
 
 use Solo\Core\Route;
+use Solo\Core\Request;
 
 require_once "../../../../config/routing.php";
 
-$uri = $argv[1];
+$uri = @$argv[1];
 if (!$uri)
+{
 	echo "use '{$argv[0]} /test/uri' to debug";
 	exit();
+}	
+	
+$_SERVER["REQUEST_METHOD"] = "GET";
+
+echo "\n\n" . str_repeat("-", 40) . "\n";
+echo "Begin debugging routing for URI: {$uri}\n";
+echo str_repeat("-", 40) . "\n";
 
 /** @var $route Route*/
-$route->debug($uri);
+echo "Matches with:\n\t" . $route->debug($uri);
+echo "\n\n";
+echo "Available variables:\n";
+
+foreach ($_GET as $k => $v)
+{
+	echo "\t{$k} => {$v}\n";
+}
+
+echo "\n\n";
